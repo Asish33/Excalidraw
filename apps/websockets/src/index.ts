92836f712a82260c3401ae4c18c7316c33f0356b
Balaji Asish <balajiashish02@gmail.com>
2025-04-06 19:57:06 +0530

    adding room logic in websockets

--- apps/websockets/src/index.ts
+++ apps/websockets/src/index.ts
@@ -1,7 +1,23 @@
-import { WebSocketServer } from "ws";
-import jwt from "jsonwebtoken";
+import { WebSocket, WebSocketServer } from "ws";
+import jwt, { JwtPayload } from "jsonwebtoken";
+//@ts-ignore
 import { JWT_SECRET } from "@repo/backend-common/config";
+//import { prismaClient } from "@repo/db/client";
 const wss = new WebSocketServer({ port: 8080 });
+interface Users {
+  ws: WebSocket;
+  rooms: string[];
+  id: string;
+}
+const users: Users[] = [];
+
+function TokenCheck(token: string): string | null {
+  const decoded = jwt.verify(token, JWT_SECRET) as JwtPayload;
+  if (!decoded || !decoded.id) {
+    return null;
+  }
+  return decoded.id;
+}
 
 wss.on("connection", function connection(ws, request) {
   const url = request.url;
@@ -11,12 +27,39 @@ wss.on("connection", function connection(ws, request) {
 
   const params = new URLSearchParams(url.split("?")[1]);
   const token = params.get("token") || "";
-  const decoded = jwt.verify(token, JWT_SECRET);
-  if (!decoded) {
+  const userId = TokenCheck(token);
+  if (!userId) {
     ws.close();
     return;
   }
-  ws.on("message", () => {
-    ws.send(token as string);
+
+  users.push({
+    ws,
+    rooms: [],
+    id: userId,
+  });
+
+  ws.on("message", (data) => {
+    const parsedData = JSON.parse(data as unknown as string);
+    if (parsedData.type === "join_room") {
+      const user = users.find((x) => x.id === userId);
+      user?.rooms.push(parsedData.roomId);
+    }
+
+    if (parsedData.type === "leave_room") {
+      const user = users.find((x) => x.id === userId);
+      if (!user) return;
+      user.rooms = user.rooms.filter((x) => x !== parsedData.roomId);
+    }
+
+    if (parsedData.type === "chat") {
+      const { roomId, message } = parsedData;
+
+      users.forEach((user) => {
+        if (user.rooms.includes(roomId)) {
+          user.ws.send(message);
+        }
+      });
+    }
   });
 });
